81bcadef7ca2e9248091940154a34ae8e1c488e0
aaaXorn <lcrs.rp@gmail.com>
2022-11-24 10:43:37 -0300

    backup

--- Assets/ScriptableObjects/Conditions/ConditionChase.cs
+++ Assets/ScriptableObjects/Conditions/ConditionChase.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class ConditionChase : StateCondition
 {
+    [SerializeField] LayerMask _playerLM = (1 << 3);
+
+    [SerializeField] float _aggroDist = 10f;
+
+    [SerializeField] float _rcDist = 10f;
+
     public override bool Condition(StateMachine sm)
     {
+        Collider[] hit = Physics.OverlapSphere(sm.transform.position, _aggroDist, _playerLM);
+        
+        if(hit.Length > 0)
+        {
+            Vector3 origin = sm.transform.position;
+            Vector3 dir = Vector3.zero;
+
+            foreach(Collider col in hit)
+            {
+                dir = (col.transform.position - origin).normalized;
+
+                //if(Raycast(origin, dir, _rcDist, _playerLM))
+            }
+        }
+
         return false;
     }
 }
